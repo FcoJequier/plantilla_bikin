2a746a1ff113bdbd3efcfccaae923f46200088dc
luis <luis.m.pinilla21@gmail.com>
2022-05-23 10:00:19 -0400

    Modificacion de la seccion servicios

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -208,6 +208,45 @@
           </div>
       </section><!-- End Portfolio Section -->
 
+      <!-- ======= Services Section ======= -->
+      <section id="services" class="services">
+          <div class="container" data-aos="fade-up">
+
+              <div class="section-title">
+                  <h2>Servicios</h2>
+                  <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+              </div>
+
+              <div class="row">
+                  <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+                      <div class="icon-box">
+                          <div class="icon"><i class='bx bxs-video'></i></div>
+                          <h4 class="title"><a href="">Atención Online</a></h4>
+                          <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
+                      <div class="icon-box">
+                          <div class="icon"><i class='bx bxs-user'></i></div>
+                          <h4 class="title"><a href="">Atención Presencial</a></h4>
+                          <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="300">
+                      <div class="icon-box">
+                          <div class="icon"><i class='bx bx-home' ></i></div>
+                          <h4 class="title"><a href="">Atención Domiciliaria</a></h4>
+                          <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                      </div>
+                  </div>
+
+              </div>
+
+          </div>
+      </section><!-- End Services Section -->
+
     <!-- ======= Testimonials Section ======= -->
     <section id="testimonials" class="testimonials color-testimonio ">
       <div class="container" data-aos="fade-up">
