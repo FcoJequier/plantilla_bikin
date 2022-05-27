c2de7fbe9cbc0bbbb42d7c3221e9eab19204239c
Francisco Jequier <francisco.jequier@gmail.com>
2022-05-27 13:53:22 -0400

    Pequeños ajustes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -90,7 +90,9 @@
             <div class="content">
               <h3>Quiénes Somos</h3>
               <p>
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."              </p>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum." </p>
               <a href="#" class="about-btn">Acerca de Nosotros <i class="bx bx-chevron-right"></i></a>
             </div>
           </div>
